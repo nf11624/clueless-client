f6cf75b30b10f4168d3f7e82a4b0165f520b682c
tom <nf11624@gmail.com>
2018-12-11 14:07:25 -0800

    80% solution complete

--- clueless-client/WebContent/client.js
+++ clueless-client/WebContent/client.js
@@ -3,10 +3,15 @@
  */
 var activePlayers = [1,2];//,3,4,5,6];
 var currentPlayer = 1;
+var gameOver = false;
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+};
 
 $(document).ready(function() {
 	initGame();
-	
+
 });
 
 updateStatus = function(status) {
@@ -38,9 +43,6 @@ updateStatus = function(status) {
 	});
 }
 
-func1 = function() {
-	updateStatus("Some message could go here?");
-}
 
 makeMove = function() {
 	$.ajax({
@@ -51,12 +53,17 @@ makeMove = function() {
 			room : $('#moveSelect').val(),
 			direction : "0"
 		}
-	}).then(function(data) {
-		console.log(data);
-		$('#moveSelect').empty();
-		currentPlayer = activePlayers[(currentPlayer % activePlayers.length)];
-		setLegalMoves()
-	});
+	}).then(
+			function(data) {
+				updateStatus(data);
+			});
+	$('#makeMove').prop('disabled', true);
+//	.then(function(data) {
+//	console.log(data);
+//	$('#moveSelect').empty();
+//	currentPlayer = activePlayers[(currentPlayer % activePlayers.length)];
+//	setLegalMoves()
+//	});
 }
 
 initGame = function() {
@@ -71,6 +78,51 @@ initGame = function() {
 	}).then(updateStatus());
 }
 
+function suggest(accuse) {
+	var url = "http://localhost:8080/clueless/suggest";
+	var targetPlayer = $('input[name=player]:checked').val();
+	var targetWeapon = $('input[name=weapon]:checked').val();
+	if (accuse === true) {
+		url = "http://localhost:8080/clueless/accuse";
+	}
+	$.ajax({
+		method: "POST",
+		url : url,
+		data : {
+			accusingPlayer : currentPlayer,
+			accusedPlayer : targetPlayer,
+			murderWeapon : targetWeapon
+		}
+	}).then(function (data) {
+		if (accuse) {
+			$('#messages').html(data.message);
+			if (data.correct === true) {
+				// Game ends
+				gameOver = true;
+			}
+			else {
+				// Remove the current player from the active player list
+				for( var i = 0; i < activePlayers.length-1; i++) { 
+					if ( array[i] === currentPlayer) {
+						arr.splice(i, 1); 
+					}
+				}
+			}
+		}
+		else {
+			$('#messages').html("Suggestions handled properly next version");
+		}
+		console.log(data);
+		$('#makeMove').prop('disabled', true);
+		$('#suggest').prop('disabled', true);
+		$('#accuse').prop('disabled', true);
+		updateStatus();
+//		await sleep(5000);
+//		doTurn();
+	});
+
+}
+
 function setLegalMoves() {
 	$.ajax({
 		method: "GET",
@@ -82,27 +134,24 @@ function setLegalMoves() {
 		for (var i = 0; i < data['legalMoves'].length; i++) {
 			var opt = new Option(data['legalMoves'][i].name, data['legalMoves'][i].name);
 			$('#moveSelect').append(opt);
-			updateStatus();
 		}
+		$('#makeMove').prop('disabled', false);
 	});
 }
 
+function finishTurn() {
+//	console.log(data);
+	$('#moveSelect').empty();
+	currentPlayer = activePlayers[(currentPlayer % activePlayers.length)];
+	$('#messages').empty();
+	doTurn();
+}
+
 function doTurn(data) {
-	$.ajax({
-		method: "POST",
-		url: "http://localhost:8080/clueless/getStatus",
-		data: {
-			gameName: "TEST GAME FROM FF"
-		}}).then(function(data) {
-			updateStatus(data);
-			$.ajax({
-				method: "GET",
-				url: "http://localhost:8080/clueless/moves",
-				data: {
-					player : currentPlayer
-				}
-			}).then(function(data) {
-				nextTurn(data);
-			})
-		})
+	if (!gameOver) {
+		$('#suggest').prop('disabled', false);
+		$('#accuse').prop('disabled', false);
+		updateStatus();
+		setLegalMoves();
+	}
 }
